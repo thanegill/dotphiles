0f320caf6cdd672085d14431d45f4be77a48d7dc
Thane Gill <me@thanegill.com>
2016-01-04 13:19:19 -0800

    Use newer git command to update submodules

--- tools/dotphiles.py
+++ tools/dotphiles.py
@@ -177,7 +177,7 @@ def _git_submodual_update(repo_directory):
         os.chdir(repo_directory)
         print e_arrow.format('Updating submoduals...')
 
-        if os.system('git submodule update --recursive --init') is not 0:
+        if os.system('submodule foreach git pull origin master') is not 0:
             raise OSError
     else:
         raise IOError
