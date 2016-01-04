608621d7de1ac4548d448a67bcf9388c0ae05020
Thane Gill <me@thanegill.com>
2016-01-03 16:39:29 -0800

    Update zsh/libs from oh-my-zsh

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -1,21 +1,18 @@
 ## Command history configuration
-
-# Show history
-if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]; then
-    alias history='fc -fl 1'
-elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]; then
-    alias history='fc -El 1'
-elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]; then
-    alias history='fc -il 1'
-else
-    alias history='fc -l 1'
-fi
-
-if [ -z $HISTFILE ]; then
+if [ -z "$HISTFILE" ]; then
     HISTFILE=$HOME/.zsh_history
 fi
+
 HISTSIZE=10000
 SAVEHIST=10000
+
+# Show history
+case $HIST_STAMPS in
+  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+  "dd.mm.yyyy") alias history='fc -El 1' ;;
+  "yyyy-mm-dd") alias history='fc -il 1' ;;
+  *) alias history='fc -l 1' ;;
+esac
 
 setopt append_history
 setopt extended_history
